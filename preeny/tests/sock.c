168e8601fde0ac765bcea5c3c36054ec0d49d06d
situlingyun <situlingyun@gmail.com>
2017-03-28 21:23:28 +0800

    modify the demo

--- preeny/tests/sock.c
+++ preeny/tests/sock.c
@@ -8,8 +8,12 @@ int main()
 	int s = socket(AF_INET, SOCK_STREAM, 0);
 	char buf[1024];
 	FILE *zomg = fdopen(s, "w");
-
+        
 	assert(send(s, "HI!\n", 4, 0) == 4);
-	assert(recv(s, buf, 1024, 0) == 5);
-	fprintf(zomg, "You wrote: %s\n", buf);
+        printf("sock id:%d\n",s);	
+        recv(s, buf, 1024, 0);
+        printf("You wrote: %s\n", buf);
+        recv(s, buf, 1024, 0);
+        printf("You wrote second: %s\n", buf);
+
 }
